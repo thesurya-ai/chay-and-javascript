759d0299698c1171b983a9c8355b9cfb99a68e31
Prakashsurya-12 <163433096+Prakashsurya-12@users.noreply.github.com>
2026-03-11 00:59:41 +0530

    learn to function in js

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -46,7 +46,46 @@ function loginUserMassage(username){
     return `${username} just logged in`
 
 }
-console.log(loginUserMassage("hitesh"));
-console.log(loginUserMassage(""));
-console.log(loginUserMassage());
+// console.log(loginUserMassage("hitesh"));
+// console.log(loginUserMassage(""));
+// console.log(loginUserMassage());
 
+function calculateCartPrice(...num1){
+    return num1 
+}
+// console.log(calculateCartPrice(200, 300, 400));  // (...) triple dott ko rest and spred oprator bola jata hai
+
+const user = {
+    username: "hitesh",
+    prices: 199
+}
+
+function handelObject(anyobject){
+    console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);
+    
+
+}
+
+// then function call 
+
+// handelObject(user)
+
+handelObject({
+    username: "Surya Prakash",
+    price: 199
+})
+
+// pass the array, using in array 
+
+const myNewArray = [200, 400, 100, 600]
+
+// then define function jo array ko accept krta hai aur uski second value ko return kar deta hai
+
+function returnSecondValue(getArray){
+    return getArray[1]
+
+}
+
+// console.log(returnSecondValue(myNewArray));
+
+console.log(returnSecondValue([100, 200, 3000, 500]));
