1a7d81cc1b3752b647843420b67052080830a768
Prakashsurya-12 <163433096+Prakashsurya-12@users.noreply.github.com>
2026-03-20 00:13:33 +0530

    scope.js

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,14 +1,31 @@
 let a = 300  // globle scope 
-if (true) {
-    let a = 10
-    const b = 20   //INNER Scope ke liye console.log
-    console.log("INNER:", a);
-    
-}
+// if (true) {
+//     let a = 10
+//     const b = 20   //INNER Scope ke liye console.log
+//     console.log("INNER:", a);  
+// }
 // console.log(a);  //globle scope ke liye console.log
 
 
 
-//Nested Scope
+// function statement / function declaration 
+function outer(){
+    let number = 0;
+
+    return function inner(){
+        return number++;
+    }
+}
+
+let result = outer();
+
+console.log(result());
+console.log(result());
+console.log(result());
+console.log(result());
+
+
+
+
 
 
---
+++ 03_basics/03_arrow.js
@@ -0,0 +1,107 @@
+// const user = {
+//     username : "hitesh",
+//     price : 999,
+
+//     welcomeMassage: function() {
+//         console.log(`${this.username}, welcome to my website`);  // this is current contest  "this keyword current contest ko refer karta hai"
+//         console.log(this);
+        
+//     }
+// }
+
+// user.welcomeMassage()
+
+/************change the current contest means user name ka value*****/
+
+// user.username = "Surya"
+// user.welcomeMassage()
+
+/******OUT PUT
+ * hitesh, welcome to my website
+{
+  username: 'hitesh',
+  price: 999,
+  welcomeMassage: [Function: welcomeMassage]   // first contest
+}
+
+
+
+Surya, welcome to my website
+{
+  username: 'Surya',
+  price: 999,
+  welcomeMassage: [Function: welcomeMassage]  //second contest
+} */
+
+
+// Using this keywords
+
+
+  const user = {
+    username : "hitesh",
+    price : 999,
+
+    welcomeMassage: function() {
+        console.log(`${this.username}, welcome to my website`);  // this is current contest  "this keyword current contest ko refer karta hai"
+        console.log(this);
+        
+    }
+}
+
+// user.welcomeMassage()
+
+/************change the current contest means user name ka value*****/
+
+// user.username = "Surya"
+// user.welcomeMassage()
+
+// console.log(this);
+
+
+// function chai(){
+//     let username = "Siddhant"
+//     console.log(this.username);
+    
+// }
+//  chai()
+
+
+
+// const chai = function() {
+//     let username = "Siddhant"
+//     console.log(this.username);  
+// }
+//  chai()  // out put undefiend beacause 
+
+
+/****using Arrow Function */
+
+const chai = () => {
+    let username = "Siddhant"
+    console.log(this);  
+}
+ chai()
+
+
+
+ //() => {} this is Arrow Function if we are use the  {}  to hme return keyword likhna padega
+
+// const addTwo = (num1, num2) =>{
+//     return num1 + num2
+// }
+// console.log(addTwo(3, 8));
+
+
+
+/****implisit return method */
+//isme {}  use nhi karte hai yah apne aap Asume kar leta hai ek hee line me likhna hota hai
+
+// const addTwo = (num1, num2) =>  num1 + num2
+
+//using () , aise bhi use kar sakte hai
+
+const addTwo = (num1, num2) =>  ( num1 + num2)
+
+
+console.log(addTwo(3, 8)); 
+
