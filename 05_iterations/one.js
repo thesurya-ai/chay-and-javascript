3fc1898058821fa7c07925519c228d0f16dafeff
Prakashsurya-12 <163433096+Prakashsurya-12@users.noreply.github.com>
2026-04-21 23:41:59 +0530

    while_do_while_loop.js

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -20,15 +20,73 @@
 
 // NESTED LOOP
 
-for (let i = 1; i <= 10; i++) {
-    console.log(`outer loop value: ${i}`); 
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`outer loop value: ${i}`); 
     
-    for (let j = 1; j <= 10; j++) {
-        // console.log(`inner loop value: ${j}, inner loop value ${i}`); 
-        console.log(i + '*' + j + '=' + i*j);
-            
-        
-        
-    }
+//     for (let j = 1; j <= 10; j++) {
+//         // console.log(`inner loop value: ${j}, inner loop value ${i}`); 
+//         console.log(i + '*' + j + '=' + i*j);
+//     }
+    
+// }
+
+let myArray = ["flash", "batman", "superman", "shaktiman"]
+    // console.log(myArray.length);
+    
+
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    //console.log(element);
+    
     
 }
+
+// break and continue
+
+//BREAKE CONDITION
+
+
+// for (let index = 1; index <= 20; index++) {
+//     if (index == 5) {
+//         console.log(`detected 5`);
+//         break  // index  value tk ruk jati hai break ka use krte hai 
+        
+//     }
+//     console.log(`value of i is ${index}`);
+
+//CONTINEU CONDITION
+    
+    
+// }
+// for (let index = 1; index <= 20; index++) {
+//     if (index == 5) {
+//         console.log(`detected 5`);
+//         continue// iska use detect ke baad tak rhta hai 
+
+//     }
+//     console.log(`value of i is ${index}`);
+    
+    
+// }
+
+// break - Loop ko turant rok deta hai (exit kar deta hai)
+
+for (let i = 1; i <= 5; i++) {
+  if (i === 3) {
+    break;
+  }
+  console.log(i);
+}
+
+// contineu - Current iteration skip karta hai, next iteration pe chala jata hai
+
+for (let i = 1; i <= 5; i++) {
+  if (i === 3) {
+    continue;
+  }
+  console.log(i);
+}
+
+// Real-life Example
+// break = class chhod ke ghar chale jaana 😄
+// continue = ek question skip karke next solve karna
---
+++ 05_iterations/while_do_while_loop.js
@@ -0,0 +1,41 @@
+//while loop
+//Pehle condition check hoti hai, fir code run hota hai
+        //while (condition) {
+          // code
+        // }
+
+
+
+let index = 0;
+while (index <= 10) {
+    // console.log(`value of the index ${index}`);
+    index  = index + 2    
+}
+// array in while loop
+    
+
+let myArray = ["flash", "batman", "superman", "shaktiman"]
+let arr = 0
+while (arr< myArray.length) {
+    // console.log(`value is ${myArray[arr]}`);
+    arr = arr+1
+    
+    
+}
+
+// do while loop isme phle kaam hoha fir condition check hoga
+// Pehle code chalega, fir condition check hogi
+// Difference (easy way)
+// while → check first, run later
+// do...while → run first, check later
+
+let score = 1
+
+do {
+    console.log(`score is ${score}`);
+    score++
+    
+
+}while (score <10);
+
+    
